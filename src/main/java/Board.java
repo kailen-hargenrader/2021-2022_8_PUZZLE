eb01b6d7b9109df17f83fab6cb59a7bcd2938cc1
kailen-hargenrader <khargenrader@maret.org>
2022-03-16 23:44:50 -0400

    feat : Implement Board Class With Tests

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,43 +1,216 @@
-public class Board {
-    public Board(int[][] blocks) {
-      // construct a board from an n-by-n array of blocks
-      // (where blocks[i][j] = block in row i, column j)
-    }
-    
-    public int dimension() {
-      // board dimension n
-      return 0;
-    }
-    public int hamming() {
-      // number of blocks out of place
-      return 0;
-    }
-    public int manhattan() {
-      // sum of Manhattan distances between blocks and goal
-      return 0;
-    }
-    public boolean isGoal() {
-      // is this board the goal board?
-      return false;
-    }
-    public Board twin() {
-      // a board that is obtained by exchanging any pair of blocks
-      return null;
-    }
-    public boolean equals(Object y) {
-      // does this board equal y?
-      return false;
-    }
-    public Iterable<Board> neighbors() {
-      // all neighboring boards
-      return null;
-    }
-    public String toString() {
-      // string representation of this board (in the output format specified below)
-      return null;
-    }
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-      // unit tests (not graded)
-    }
+public class Board {
+	private int[][] Blocks;
+	public Board(int[][] blocks) {
+		// construct a board from an n-by-n array of blocks
+		// (where blocks[i][j] = block in row i, column j)
+		if(blocks == null) throw new IllegalArgumentException("blocks cannot be null");
+		Blocks = new int[blocks.length][blocks.length];
+		Blocks = blocks.clone();
+	}
+
+	public int dimension() {
+		// board dimension n
+		return Blocks.length;
+	}
+	public int hamming() {
+		// number of blocks out of place
+		int count = 1;
+		int num = 0;
+		for(int i =0;i<dimension();i++) {
+			for(int j =0; j<dimension(); j++) {
+				if(Blocks[i][j] != 0 && Blocks[i][j] != count) {
+					num++;	
+				}
+				count++;
+			}
+		}
+		return num;
+	}
+	public int manhattan() {
+		// sum of Manhattan distances between blocks and goal
+		int count = 1;
+		int num = 0;
+		for(int i = 0;i<dimension();i++) {
+			for(int j = 0; j<dimension(); j++) {
+				if(Blocks[i][j] != 0 && Blocks[i][j] != count) {
+					if ((Blocks[i][j]-1) % dimension() - j < 0) {
+						num = num - ((Blocks[i][j]-1) % dimension() - j);
+					}
+					else {
+						num = num + ((Blocks[i][j]-1) % dimension() - j);
+					}
+					if((Blocks[i][j]-1) / dimension() - i < 0) {
+						num = num - ((Blocks[i][j]-1) / dimension() - i);
+					}
+					else {
+						num = num + ((Blocks[i][j]-1) / dimension() - i);
+					}
+				}
+				count++;
+			}
+
+		}
+		return num;
+	}
+
+	public boolean isGoal() {
+		// is this board the goal board?
+		int count = 1;
+		for(int[] i : Blocks) {
+			for(int j : i) {
+				if(j != count)return false;
+				if(count == Blocks.length*Blocks.length-1) return true;
+				count++;
+			}
+		}
+		throw new ArithmeticException("How did you get here?");
+	}
+	public Board twin() {
+		// a board that is obtained by exchanging any pair of blocks
+		if(Blocks.length == 1 || Blocks.length == 0) throw new NullPointerException("No blocks to exchange.");
+		int[][] temp = new int[Blocks.length][Blocks.length];
+		temp = Blocks.clone();
+		if(temp[0][0] == 0 || temp [0][1] == 0) {
+			int Int = temp[1][0];
+			temp[1][0] = temp[1][1];
+			temp[1][1] = Int;
+		}
+		else {
+			int Int = temp[0][0];
+			temp[0][0] = temp[0][1];
+			temp[0][1] = Int;
+		}
+		return new Board(temp);
+	}
+	public boolean equals(Object y) {
+		// does this board equal y?
+		if(this == y) return true;
+		if(y == null) return false;
+		if(y.getClass() == this.getClass()) {
+			Board newY = (Board) y;
+			return newY.dimension() == this.dimension() && Arrays.deepEquals(newY.Blocks, this.Blocks);
+		}
+		return false;
+	}
+	public Iterable<Board> neighbors() {
+		// all neighboring boards
+		int row = 0;
+		int col = 0;
+		Board[] boards;
+		while (Blocks[row][col] != 0) { 
+			if(col != dimension()-1) col++;
+			else {
+				row++;
+				col = 0;
+			}
+		}
+		if(row != 0 && row != dimension()-1) {
+			if(col != 0 && col != dimension()-1){
+				boards = new Board[4];
+				boards[1] = new Board(Blocks);
+				boards[1].Blocks[row][col] = boards[1].Blocks[row-1][col];
+				boards[1].Blocks[row-1][col] = 0;
+				
+				boards[2] = new Board(Blocks);
+				boards[2].Blocks[row][col] = boards[2].Blocks[row+1][col];
+				boards[2].Blocks[row+1][col] = 0;
+				
+				boards[3] = new Board(Blocks);
+				boards[3].Blocks[row][col] = boards[1].Blocks[row][col+1];
+				boards[3].Blocks[row][col+1] = 0;
+				
+				boards[0] = new Board(Blocks);
+				boards[0].Blocks[row][col] = boards[1].Blocks[row][col-1];
+				boards[0].Blocks[row][col-1] = 0;
+			}
+			else {
+				boards = new Board[3];
+				boards[1] = new Board(Blocks);
+				boards[1].Blocks[row][col] = boards[1].Blocks[row-1][col];
+				boards[1].Blocks[row-1][col] = 0;
+				
+				boards[2] = new Board(Blocks);
+				boards[2].Blocks[row][col] = boards[2].Blocks[row+1][col];
+				boards[2].Blocks[row+1][col] = 0;
+				
+				if(col == 0) {
+					boards[0] = new Board(Blocks);
+					boards[0].Blocks[row][col] = boards[1].Blocks[row][col+1];
+					boards[0].Blocks[row][col+1] = 0;
+				}
+				else {
+					boards[0] = new Board(Blocks);
+					boards[0].Blocks[row][col] = boards[1].Blocks[row][col-1];
+					boards[0].Blocks[row][col-1] = 0;
+				}
+				
+				
+			}
+		}
+		else {
+			if(col != 0 && col != dimension()-1) {
+				boards = new Board[3];
+				if(row == 0) {
+					boards[1] = new Board(Blocks);
+					boards[1].Blocks[row][col] = boards[1].Blocks[row+1][col];
+					boards[1].Blocks[row+1][col] = 0;
+				}
+				else {
+					boards[1] = new Board(Blocks);
+					boards[1].Blocks[row][col] = boards[1].Blocks[row-1][col];
+					boards[1].Blocks[row-1][col] = 0;
+				}
+				
+				boards[2] = new Board(Blocks);
+				boards[2].Blocks[row][col] = boards[1].Blocks[row][col+1];
+				boards[2].Blocks[row][col+1] = 0;
+				
+				boards[0] = new Board(Blocks);
+				boards[0].Blocks[row][col] = boards[1].Blocks[row][col-1];
+				boards[0].Blocks[row][col-1] = 0;
+			}
+			else {
+				boards = new Board[2];
+				if(row == 0) {
+					boards[1] = new Board(Blocks);
+					boards[1].Blocks[row][col] = boards[1].Blocks[row+1][col];
+					boards[1].Blocks[row+1][col] = 0;
+				}
+				else {
+					boards[1] = new Board(Blocks);
+					boards[1].Blocks[row][col] = boards[1].Blocks[row-1][col];
+					boards[1].Blocks[row-1][col] = 0;
+				}
+				if(col == 0) {
+					boards[0] = new Board(Blocks);
+					boards[0].Blocks[row][col] = boards[1].Blocks[row][col+1];
+					boards[0].Blocks[row][col+1] = 0;
+				}
+				else {
+					boards[0] = new Board(Blocks);
+					boards[0].Blocks[row][col] = boards[1].Blocks[row][col-1];
+					boards[0].Blocks[row][col-1] = 0;
+				}
+			}
+		}
+		return Arrays.asList(boards);
+	}
+	public String toString() {
+		// string representation of this board (in the output format specified below)
+		StringBuilder s = new StringBuilder();
+		s.append(dimension() + "\n");
+		for (int i = 0; i < dimension(); i++) {
+			for (int j = 0; j < dimension(); j++) {
+				s.append(String.format("%2d ", Blocks[i][j]));
+			}
+			s.append("\n");
+		}
+		return s.toString();
+	}
+
+	public static void main(String[] args) {
+		// unit tests (not graded)
+	}
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -9,45 +9,76 @@ import org.junit.Test;
 import edu.princeton.cs.algs4.In;
 
 public class BoardTest {
-  
-  Board board;
-  @Before
-  public void setUp() throws Exception {
-    board = generateBoard("input10.txt");
-  }
-  
-  private Board generateBoard(String filename) {
- // create initial board from file
-    In in = new In("8puzzle-test-files/" + filename);
-    int n = in.readInt();
-    int[][] blocks = new int[n][n];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            blocks[i][j] = in.readInt();
-    Board initial = new Board(blocks);
 
-    // solve the puzzle
-    Solver solver = new Solver(initial);
+	Board board;
+	Board board2;
+	@Before
+	public void setUp() throws Exception {
+		board = generateBoard("puzzle00.txt");
+		board2 = generateBoard("puzzle01.txt");
+	}
 
-    // print solution to standard output
-//    if (!solver.isSolvable())
-//        StdOut.println("No solution possible");
-//    else {
-//        StdOut.println("Minimum number of moves = " + solver.moves());
-//        for (Board board : solver.solution())
-//            StdOut.println(board);
-//    }
-    return initial;
-  }
+	private Board generateBoard(String filename) {
+		// create initial board from file
+		In in = new In("8puzzle-test-files/" + filename);
+		int n = in.readInt();
+		int[][] blocks = new int[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				blocks[i][j] = in.readInt();
+		Board initial = new Board(blocks);
 
-  @Test
-  public void testDimension() {
-    //fail("Not yet implemented");
-  }
+		// solve the puzzle
+		Solver solver = new Solver(initial);
 
-  @Test
-  public void testSegments() {
-    //fail("Not yet implemented");
-  }
+		// print solution to standard output
+		//    if (!solver.isSolvable())
+		//        StdOut.println("No solution possible");
+		//    else {
+		//        StdOut.println("Minimum number of moves = " + solver.moves());
+		//        for (Board board : solver.solution())
+		//            StdOut.println(board);
+		//    }
+		return initial;
+	}
 
+	@Test
+	public void testDimension() {
+		assertTrue("dimension should be 10, was " + board.dimension(), board.dimension() == 10);
+	}
+
+	@Test
+	public void testHamming() {
+		assertTrue("hamming should be 0, was " + board.hamming(), board.hamming() == 0);
+	}
+
+	@Test
+	public void testManhattan() {
+		assertTrue("Manhattan should be 0, was " + board.manhattan(), board.manhattan() == 0);
+	}
+
+	@Test
+	public void testGoal() {
+		assertTrue("Goal should be true, was " + board.isGoal(), board.isGoal() == true);
+	}
+
+	@Test
+	public void testTwin() {
+		Board twin = board.twin();
+		System.out.println(twin);
+		assertTrue("Hamming of Twin should be 2, was " + twin.hamming(), twin.hamming() == 2);
+		assertTrue("manhattan of Twin should be 2, was " + twin.manhattan(), twin.manhattan() == 2);
+	}
+
+	@Test
+	public void testEquals() {
+		int[][] Nums = new int[2][2];
+		Nums[0][0] = 1;
+		Nums[0][1] = 0;
+		Nums[1][0] = 3;
+		Nums[1][1] = 2;
+		Board clone = new Board(Nums);
+		System.out.println(clone);
+		assertTrue("boards should be equal", board2.equals(clone));
+	}
 }
